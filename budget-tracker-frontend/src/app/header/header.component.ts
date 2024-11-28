981e563d8e5bc8b93a9d56a76b46fd674e9c5a11
JilbertVasquez <jilbertvasquez@gmail.com>
2024-11-28 22:33:28 +0800

    create header

---
+++ budget-tracker-frontend/src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+
+@Component({
+    selector: 'app-header',
+    imports: [MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule, CommonModule, RouterModule],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
