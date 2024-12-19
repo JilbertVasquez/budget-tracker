c1530e97e5e34a6d92139cd27c6807123d9a0aa6
JilbertVasquez <jilbertvasquez@gmail.com>
2024-12-19 11:36:29 +0800

    initial create expense

--- budget-tracker-frontend/src/app/expenses/create-expenses/create-expenses.component.ts
+++ budget-tracker-frontend/src/app/expenses/create-expenses/create-expenses.component.ts
@@ -1,11 +1,63 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create-expenses',
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, MatButtonModule, FormsModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './create-expenses.component.html',
   styleUrl: './create-expenses.component.css'
 })
 export class CreateExpensesComponent {
+    createForm: FormGroup;
+    isBusy = false;
+    withNote = false;
+    withCategory = false;
 
+    constructor() {
+        this.createForm = new FormGroup({
+            name: new FormControl('',
+                [
+                    Validators.required,
+                    Validators.minLength(2),
+                ]
+            ),
+            description: new FormControl('',
+                [
+                    Validators.required,
+                    Validators.minLength(2),
+                ]
+            ),
+            note: new FormControl('',
+                [
+                    Validators.minLength(2)
+                ]
+            ),
+            amount: new FormControl('',
+                [
+                    Validators.required
+                ]
+            ),
+            category: new FormControl('',
+                [
+                    Validators.minLength(2),
+                ]
+            )
+        });
+    }
+
+    toggleNote() {
+        this.withNote = !this.withNote;
+    }
+
+    toggleCategory() {
+        this.withCategory = !this.withCategory;
+    }
+
+    async submit() {
+
+    }
 }
