b008b5023d163b6f8a17812bcd0242c2dcad5706
JilbertVasquez <jilbertvasquez@gmail.com>
2024-12-20 16:09:50 +0800

    try date picker filter

--- budget-tracker-frontend/src/app/_dtos/expenses/expenses-details-dto.ts
+++ budget-tracker-frontend/src/app/_dtos/expenses/expenses-details-dto.ts
@@ -8,5 +8,5 @@ export interface ExpenseDetailsDto {
     category?: string;
     amount: number;
     period: PeriodDto;
-    createAt: string
+    createdAt: string
 }
--- budget-tracker-frontend/src/app/expenses/expenses-list/expenses-list.component.ts
+++ budget-tracker-frontend/src/app/expenses/expenses-list/expenses-list.component.ts
@@ -7,18 +7,23 @@ import { DialogService } from '../../_services/dialog.service';
 import { ErrorService } from '../../_services/error.service';
 import { Router } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { FormsModule } from '@angular/forms';
 @Component({
     selector: 'app-expenses-list',
-    imports: [MatCardModule, DataTableComponent],
+    imports: [MatCardModule, DataTableComponent, MatFormFieldModule, MatDatepickerModule, FormsModule],
     templateUrl: './expenses-list.component.html',
-    styleUrl: './expenses-list.component.css'
+    styleUrl: './expenses-list.component.css',
+    providers: [provideNativeDateAdapter()],
 })
 export class ExpensesListComponent implements OnInit, OnDestroy, AfterViewInit {
     @ViewChild('dt') dt!: DataTableComponent<ExpenseDetailsDto>;
     isLoading = false;
     isBusy = false;
     data: ExpenseDetailsDto[] = [];
+    dateRange: { start: Date | null, end: Date | null } = { start: null, end: null };
     columns: Column[] = [
         // { identifier: 'expenseId', title: 'Id' },
         { identifier: 'name', title: 'Name' },
@@ -49,6 +54,19 @@ export class ExpensesListComponent implements OnInit, OnDestroy, AfterViewInit {
         });
     }
 
+    onDateRangeChange(): void {
+        if (this.dateRange.start && this.dateRange.end) {
+            const filteredData = this.data.filter(x => {
+                const createdAt = new Date(x.createdAt);
+                return createdAt >= this.dateRange.start! && createdAt <= this.dateRange.end!;
+            });
+            this.dt.dataSource.data = filteredData;
+        }
+        else {
+            this.dt.dataSource.data = this.data;
+        }
+    }
+
     editExpense(data: ExpenseDetailsDto) {
         this._router.navigate(['expenses/expenses-details/', data.expenseId]);
     }
@@ -57,7 +75,7 @@ export class ExpensesListComponent implements OnInit, OnDestroy, AfterViewInit {
         try {
             const isConfirm = await this._getUserConfirmation(`Do you want to delete ${data.name} expense?`);
             if (!isConfirm) return;
-            
+
             await this._expensesService.deleteExpense(data.expenseId);
             this._dialogService.message("Expense successfully deleted.");
             await this._expensesService.loadExpensesList();
@@ -70,7 +88,8 @@ export class ExpensesListComponent implements OnInit, OnDestroy, AfterViewInit {
 
     private _loadData() {
         this.data = this._expensesService.expensesList();
-        this.dt.dataSource.data = this.data;
+        this.onDateRangeChange();
+        // this.dt.dataSource.data = this.data;
     }
 
     private async _getUserConfirmation(message: string) {
