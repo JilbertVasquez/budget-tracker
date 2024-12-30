7b7fc597be8fb4fb1d91d7c313efd7fcb5cc1292
JilbertVasquez <jilbertvasquez@gmail.com>
2024-12-30 15:51:11 +0800

    navigation buttons for savings feature

--- budget-tracker-frontend/src/app/savings/savings-routing.module.ts
+++ budget-tracker-frontend/src/app/savings/savings-routing.module.ts
@@ -3,6 +3,7 @@ import { SavingsHomeComponent } from "./savings-home/savings-home.component";
 import { SavingsListComponent } from "./savings-list/savings-list.component";
 import { SavingsDetailsComponent } from "./savings-details/savings-details.component";
 import { CreateSavingsComponent } from "./create-savings/create-savings.component";
+import { WithdrawSavingsComponent } from "./withdraw-savings/withdraw-savings.component";
 
 
 export default [
@@ -11,15 +12,19 @@ export default [
         component: SavingsHomeComponent
     },
     {
-        path: 'expenses-list',
+        path: 'savings-list',
         component: SavingsListComponent,
     },
     {
-        path: 'expenses-details/:id',
+        path: 'savings-details/:id',
         component: SavingsDetailsComponent,
     },
     {
-        path: 'create-expenses',
+        path: 'create-savings',
         component: CreateSavingsComponent,
+    },
+    {
+        path: 'withdraw-savings',
+        component: WithdrawSavingsComponent,
     },
 ] as Routes;
---
+++ budget-tracker-frontend/src/app/savings/withdraw-savings/withdraw-savings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-withdraw-savings',
+  imports: [],
+  templateUrl: './withdraw-savings.component.html',
+  styleUrl: './withdraw-savings.component.css'
+})
+export class WithdrawSavingsComponent {
+
+}
