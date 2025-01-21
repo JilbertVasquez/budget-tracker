acc508569a0b0bc45a3daa07cf15c5e787018906
jilbertvasquez <jilbertvasquez@gmail.com>
2025-01-21 17:10:13 +0800

    implement has permission

--- budget-tracker-frontend/src/app/_guards/expense-tracker.guard.ts
+++ budget-tracker-frontend/src/app/_guards/expense-tracker.guard.ts
@@ -8,7 +8,7 @@ export const expenseTrackerGuard: CanActivateFn = () => {
     const authService = inject(AuthService);
     const dialogService = inject(DialogService);
 
-    if (!authService.getUserRoles().includes(UserRole.ExpenseTracker)) {
+    if (!authService.hasPermission(UserRole.ExpenseTracker)) {
         dialogService.message("You don't have enough permission.");
         return false;
     }
--- budget-tracker-frontend/src/app/_guards/savings.guard.ts
+++ budget-tracker-frontend/src/app/_guards/savings.guard.ts
@@ -8,7 +8,7 @@ export const savingsGuard: CanActivateFn = () => {
     const authService = inject(AuthService);
     const dialogService = inject(DialogService);
 
-    if (!authService.getUserRoles().includes(UserRole.Saver)) {
+    if (!authService.hasPermission(UserRole.Saver)) {
         dialogService.message("You don't have enough permission.");
         return false;
     }
