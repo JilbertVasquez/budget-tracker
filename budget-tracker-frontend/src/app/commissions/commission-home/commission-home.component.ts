53d52d97a6546ab7840c99e451871f34e0d149a5
jilbertvasquez <jilbertvasquez@gmail.com>
2025-01-21 21:26:06 +0800

    configure commission routing

--- budget-tracker-frontend/src/app/commissions/commission-home/commission-home.component.ts
+++ budget-tracker-frontend/src/app/commissions/commission-home/commission-home.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-commission-home',
-  imports: [],
-  templateUrl: './commission-home.component.html',
-  styleUrl: './commission-home.component.css'
+    selector: 'app-commission-home',
+    imports: [RouterModule, MatButtonModule],
+    templateUrl: './commission-home.component.html',
+    styleUrl: './commission-home.component.css',
 })
-export class CommissionHomeComponent {
-
-}
+export class CommissionHomeComponent {}
