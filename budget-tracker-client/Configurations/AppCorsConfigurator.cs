4aa48b8d5b477ef3b24846f3125839acefdee3c0
JilbertVasquez <jilbertvasquez@gmail.com>
2025-06-26 21:33:24 +0800

    revert

--- budget-tracker-client/Configurations/AppCorsConfigurator.cs
+++ budget-tracker-client/Configurations/AppCorsConfigurator.cs
@@ -19,7 +19,7 @@ public static class AppCorsConfigurator
         builder.Services.AddCors(x => x.AddPolicy(_productionCorsPolicy, CorsPolicyBuilder =>
         {
             CorsPolicyBuilder
-                .WithOrigins("https://budgettracker.io", "http://budgettracker.io", "http://localhost:5004", "https://budget-tracker-app.victoriousplant-30209041.southeastasia.azurecontainerapps.io/")
+                .WithOrigins("https://budgettracker.io", "http://budgettracker.io", "http://localhost:5004")
                 .AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials();
