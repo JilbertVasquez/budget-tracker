b00ccc4d13a7ae9c8c90b22fdd54c50bf569461f
JilbertVasquez <jilbertvasquez@gmail.com>
2025-06-26 21:57:24 +0800

    frontend env prod config

--- budget-tracker-frontend/src/environments/environment.prod.ts
+++ budget-tracker-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5004',
+    production: true,
+    apiUrl: 'https://budget-tracker-app.victoriousplant-30209041.southeastasia.azurecontainerapps.io/',
     auth0ClientId: 'JR11kAHNDAaIJT7trDvETPGBdiyGB2WX',
     auth0Audience: 'budget-tracker-api',
     auth0Domain: 'dev-bpgsygqobozd6q0z.us.auth0.com'
