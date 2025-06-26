ad005e4fa639af985a566a92d91a2c4580c8e7ee
JilbertVasquez <jilbertvasquez@gmail.com>
2025-06-26 21:32:32 +0800

    update cors production with origins

--- budget-tracker-client/Configurations/AppCorsConfigurator.cs
+++ budget-tracker-client/Configurations/AppCorsConfigurator.cs
@@ -19,7 +19,7 @@ public static class AppCorsConfigurator
         builder.Services.AddCors(x => x.AddPolicy(_productionCorsPolicy, CorsPolicyBuilder =>
         {
             CorsPolicyBuilder
-                .WithOrigins("https://budgettracker.io", "http://budgettracker.io", "http://localhost:5004")
+                .WithOrigins("https://budgettracker.io", "http://budgettracker.io", "http://localhost:5004", "https://budget-tracker-app.victoriousplant-30209041.southeastasia.azurecontainerapps.io/")
                 .AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials();
